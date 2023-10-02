f0d4fb76473ab46164dfd73e9ecda34fab63bf61
Tirth <tirthacodes@gmail.com>
2023-10-02 18:39:27 +0545

    sum of 2 number taking from console
    two numbers has been taken from console and added

---
+++ basics/sum-of-two-numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a;
+        int b;
+        Console.Write("Enter first number: ");
+        a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter second number: ");
+        b = Convert.ToInt32(Console.ReadLine());
+
+        var result = a + b;
+        Console.WriteLine("Result : " + result);
+    }
+}
