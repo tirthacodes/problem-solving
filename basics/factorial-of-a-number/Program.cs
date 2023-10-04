0afadb22e2b8ceba796f42c2d353b667869c4dd4
Tirth <tirthacodes@gmail.com>
2023-10-04 21:45:11 +0545

    factorial of given number

---
+++ basics/factorial-of-a-number/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int number;
+        int result=1;
+
+        Console.Write("Enter a number to find factorial: ");
+        number = Convert.ToInt32(Console.ReadLine());
+
+        for(int i=number; i>0; i--){
+            result *= i;
+        }
+
+        Console.WriteLine($"The factorial of {number} is: " + result );
+    }
+}
