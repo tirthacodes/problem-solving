e239f7e38f71b8a867b6a8572e131d339b6d24f1
Tirth <tirthacodes@gmail.com>
2023-10-06 23:58:58 +0545

    folder shifted

---
+++ problem-solving-in-csharp/basics/sum-of-digits/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Security;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int sum =0;
+
+        Console.WriteLine("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int num = number;
+
+        while (num > 0){
+            int rem = num%10;
+            sum += rem;
+            num /= 10;
+        }
+
+        System.Console.WriteLine($"The sum of digit of number {number} is: " + sum);
+    }
+}
