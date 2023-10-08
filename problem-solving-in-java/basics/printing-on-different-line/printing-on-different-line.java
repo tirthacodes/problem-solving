ec12732d46133f43a4a1a4765bb3c9babad85e05
Tirth <tirthacodes@gmail.com>
2023-10-08 20:36:32 +0545

    printing on same and different line

---
+++ problem-solving-in-java/basics/printing-on-different-line/printing-on-different-line.java
@@ -0,0 +1,8 @@
+class Program{
+    public static void main(String[] args){
+        System.out.print("printing ");
+        System.out.print("on same line.");
+        System.out.println("printing");
+        System.out.println("on different line.");
+    }
+}
